0ab11dccfcace1b6f3d6db0db4a98876c1af2db4
Meriem Selmi <Meriem@satellite>
2013-03-03 17:14:07 +0100

    Added About us interface

--- src/tn/iac/radiostreaming/AboutActivity.java
+++ src/tn/iac/radiostreaming/AboutActivity.java
@@ -5,6 +5,7 @@ import tn.iac.radiostreaming.db.RadioChannelTable;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -19,14 +20,7 @@ public class AboutActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about);
-
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_about, menu);
-		return true;
+		setTheme(R.style.WidgetBackground);
 	}
 
 }
