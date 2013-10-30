77be44a8700e708fe74b3755efcb644e6e29c168
Meriem Selmi <selmimeem@gmail.com>
2013-10-30 20:39:37 +0100

    Fixed AboutActivity

--- src/tn/iac/radiostreaming/AboutActivity.java
+++ src/tn/iac/radiostreaming/AboutActivity.java
@@ -1,20 +1,9 @@
 package tn.iac.radiostreaming;
 
-import java.util.List;
-import tn.iac.radiostreaming.db.RadioChannelTable;
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.Window;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 public class AboutActivity extends Activity{
-	
-	RadioChannelTable radioChannels;
-	List<String> channelUrls;
-	ListView listView;
-	ArrayAdapter<String> adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
