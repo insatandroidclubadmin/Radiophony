4798e0633137e992f865cecef4a32e3e59137d6c
Meriem Selmi <selmimeem@gmail.com>
2013-11-03 17:01:24 +0100

    Changed SplashScreen

--- src/tn/iac/radiostreaming/SplashActivity.java
+++ src/tn/iac/radiostreaming/SplashActivity.java
@@ -4,16 +4,24 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashActivity extends Activity {
 
-	private static final int SPLASH_TIME = 3 * 1000;// 3 seconds
-
+	private static final int SPLASH_TIME = 2 * 1000;// 3 seconds
+	Animation animation; 
+	ImageView logo;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
-
+		animation = AnimationUtils.loadAnimation(this, R.anim.down_to_up);
+		logo = (ImageView) findViewById(R.id.splash_image);
+		logo.startAnimation(animation);
+		
 		new Handler().postDelayed(new Runnable() {
 
 			@Override
